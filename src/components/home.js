4f90444bb7be7db544ef0c7837b1fd0a8adc2a11
Shawn Levin <shawnlevin22@gmail.com>
2018-02-14 16:19:20 -0800

    created promise with redux middleware

--- src/components/home.js
+++ src/components/home.js
@@ -9,15 +9,30 @@ class Home extends Component {
     }
 
     render() {
+        console.log("Props:", this.props);
+        const listItems = this.props.todoList.map((item, index) => {
+            return <li className="list-group-item" key={index}>{item.title}</li>
+        })
+
+
         return (
             <div>
                 <div className="text-center">
                     <h1>To Do List</h1>
                     <p>Now with Redux!</p>
                 </div>
+                <ul className="list-group">
+                    {listItems}
+                </ul>
             </div>
         )
     }
 }
 
-export default connect(null, { getAllTodos })(Home);
+function mapStateToProps(state) {
+    return {
+        todoList: state.todo.all
+    }
+}
+
+export default connect(mapStateToProps, { getAllTodos })(Home);
